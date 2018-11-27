d0ef706e5d7982c827add8e3cb6afd65fcdf4240
morar <morar@DESKTOP-FS6O4A0>
2018-11-27 18:11:40 +0200

    main class

--- GuessNumber/Program.cs
+++ GuessNumber/Program.cs
@@ -10,6 +10,34 @@ namespace GuessNumber
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(100);
+            int insertedNumber = InsertNumber();
+
+            while (insertedNumber != randomNumber)
+            {
+                if (insertedNumber < randomNumber)
+                {
+                    Console.WriteLine("The inserted number is less than the generated one");
+                    insertedNumber = InsertNumber();
+                }
+                else
+                {
+                    Console.WriteLine("The inserted number is greater than the generated one");
+                    insertedNumber = InsertNumber();
+                }
+            }
+            if (insertedNumber == randomNumber)
+            {
+                Console.WriteLine("You won!");
+            }
+        }
+
+        static int InsertNumber()
+        {
+            Console.Write("Insert a number: ");
+            string number = Console.ReadLine();
+            return int.Parse(number);
         }
     }
 }
